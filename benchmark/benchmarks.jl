fc9459ddeef2c220cb1d534d416ba02bd005b9af
karei <abcdvvvv@disroot.org>
2026-07-30 22:07:27 +0900

    fix const DEFAULT in benchmarks.jl [skip ci]

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,7 +11,7 @@ Logging.catch_exceptions(::NullSinkLogger) = false
 
 import ComponentLogging as CL
 const RK = CL.RuleKey
-const DEFAULT = (CL.Default_Sym,)
+const DEFAULT = try (CL.Default_Sym,) catch; (CL.DEFAULT_SYM,) end
 
 ## Create logger
 # Explicitly add rules for: default group, :opti, (:a,:b), and (:a,:b,:c,:d,:e,:f,:g,:h)
